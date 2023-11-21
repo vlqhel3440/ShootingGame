0f01145de157279eb04452d6f1a32c7df59f0769
vlqhel3440 <vlqhel3440@naver.com>
2023-11-21 22:04:14 +0900

    add Player class

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
 const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d')
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
+
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const player = new Player(x, y, 30, 'blue')
+player.draw()
+
+console.log(player)
+
+addEventListener('click', () => {
+  
+})
