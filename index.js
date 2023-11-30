0174ee516530c34dc43930b7cac00906289991ef
vlqhel3440 <vlqhel3440@naver.com>
2023-11-30 21:41:27 +0900

    add enemy javascript

--- index.js
+++ index.js
@@ -43,26 +43,55 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+
+  update() {
+    this.draw()
+    this.x =this.x + this.velocity.x
+    this.y =this.y + this.velocity.y
+  }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue')
-player.draw()
-
-// const projectile = new Projectile(
-//   canvas.width / 2,
-//   canvas.height / 2,
-//   5,
-//   'red',
-//   {
-//     x: 1,
-//     y: 1
-//   }
-// )
 const projectiles = []
+const enemise = []
+
+function spawnEnemies() {
+  setInterval(() => {
+    const x = 100
+    const y = 100
+    const radius = 30
+    const color = 'green'
+    const velocity = {
+      x: 1,
+      y: 1
+    }
+    enemise.push(new Enemy(x, y, radius, color, velocity))
+    console.log(enemise)
+  }, 1000)
+}
 
 function animate() {
   requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  player.draw()
   projectiles.forEach((projectile) => {
     projectile.update()
   })
@@ -84,3 +113,4 @@ addEventListener('click', (event) => {
 })
 
 animate()
+spawnEnemies()
