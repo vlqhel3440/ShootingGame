1841d90de7ce5bdb272d4f92b10da240b2879ea9
vlqhel3440 <vlqhel3440@naver.com>
2023-11-30 20:12:41 +0900

    add projectiles javascript

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ class Projectile {
     ctx.fillStyle = this.color
     ctx.fill()
   }
+
+  update() {
+    this.draw()
+    this.x =this.x + this.velocity.x
+    this.y =this.y + this.velocity.y
+  }
 }
 
 const x = canvas.width / 2
@@ -43,8 +49,38 @@ const y = canvas.height / 2
 const player = new Player(x, y, 30, 'blue')
 player.draw()
 
-console.log(player)
+// const projectile = new Projectile(
+//   canvas.width / 2,
+//   canvas.height / 2,
+//   5,
+//   'red',
+//   {
+//     x: 1,
+//     y: 1
+//   }
+// )
+const projectiles = []
 
-addEventListener('click', () => {
-  
+function animate() {
+  requestAnimationFrame(animate)
+  projectiles.forEach((projectile) => {
+    projectile.update()
+  })
+}
+
+addEventListener('click', (event) => {
+  const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width /2)
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle)
+  }
+  projectiles.push(new Projectile(
+    canvas.width / 2,
+    canvas.height / 2,
+    5,
+    'red',
+    velocity
+  ))
 })
+
+animate()
