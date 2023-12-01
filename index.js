a8c76fa2fb627a3459b8b0c8e7d7b3eea734d7d2
vlqhel3440 <vlqhel3440@naver.com>
2023-12-01 20:44:05 +0900

    edit color p2

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ class Projectile {
 
   update() {
     this.draw()
-    this.x =this.x + this.velocity.x * 2
-    this.y =this.y + this.velocity.y * 2
+    this.x =this.x + this.velocity.x * 6
+    this.y =this.y + this.velocity.y * 6
   }
 }
 
@@ -69,7 +69,7 @@ class Enemy {
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 10, 'white')
 const projectiles = []
 const enemise = []
 
@@ -85,7 +85,7 @@ function spawnEnemies() {
       x = Math.random() * canvas.width
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
     }
-    const color = 'green'
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width /2 - x)
     const velocity = {
       x: Math.cos(angle),
@@ -99,11 +99,21 @@ function spawnEnemies() {
 let animationId
 function animate() {
   animationId = requestAnimationFrame(animate)
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
   player.draw()
 
-  projectiles.forEach((projectile) => {
+  projectiles.forEach((projectile, index) => {
     projectile.update()
+    //remove from edges of screen
+    if (projectile.x - projectile.radius < 0 ||
+        projectile.x - projectile.radius > canvas.width ||
+        projectile.y + projectile.radius < 0 ||
+        projectile.y - projectile.radius > canvas.height){
+      setTimeout(() => {
+        projectiles.splice(index, 1)
+      }, 0)
+    }
   })
 
   enemise.forEach((enemy, index) => {
@@ -119,8 +129,10 @@ function animate() {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
       // objects touch
       if(dist - enemy.radius - projectile.radius < 1) {
-        enemise.splice(index, 1)
-        projectiles.splice(projectileIndex, 1)
+        setTimeout(() => {
+          enemise.splice(index, 1)
+          projectiles.splice(projectileIndex, 1)
+        }, 0)
       }
     })
   })
@@ -136,7 +148,7 @@ addEventListener('click', (event) => {
     canvas.width / 2,
     canvas.height / 2,
     5,
-    'red',
+    'white',
     velocity
   ))
 })
